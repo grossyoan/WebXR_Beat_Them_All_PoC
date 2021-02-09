8e169ab404c1dd01182daee89bd9eef8189f5c6b
gross.yoan@gmail.com <32864409+grossyoan@users.noreply.github.com>
2021-02-09 11:37:27 +0100

    :sparkles: velocity and throw added

---
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] ObjectPrefab;
+    public float SpawnInterval = 10;
+
+
+    private float Timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        if (Timer >= SpawnInterval)
+        {
+            Spawn();
+            Timer = 0;
+        }
+    }
+    
+    public void Spawn()
+    {
+        int randomNb = Random.Range(0, 10);
+
+        GameObject copy;
+        copy = Instantiate(ObjectPrefab[0], transform);
+        
+    }
+}
